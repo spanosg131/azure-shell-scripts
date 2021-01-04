644b6296f360c721cab2c073a3d68c5b05163464
Georgios Spanos <georgios.spanos@ses.com>
2021-01-04 13:44:10 -0500

    removed due to deprication warning

--- terraform-blob-provisioner.sh
+++ terraform-blob-provisioner.sh
@@ -145,7 +145,6 @@ terraform {
 provider "azurerm" { 
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you are using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
   features {}
 }
 EOF
